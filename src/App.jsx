5335015d88baab1b9081f11f2776144d8a57a36c
AquaApple <yassershehab9@gmail.com>
2022-10-24 17:32:04 +0200

    Added Component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import styles from "./style";
 
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
+
 export const App = () => {
   return (
     <div className="bg-primary	w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
-        stats business billing cardDeal testimonials clients create footer
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+        </div>
+
+        <footer />
       </div>
     </div>
   );
