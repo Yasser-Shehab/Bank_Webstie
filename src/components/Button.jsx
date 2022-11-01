a837f75007764a5cd818221ca2fa152320980c80
AquaApple <yassershehab9@gmail.com>
2022-11-01 19:14:15 +0200

    Feature Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = (props) => {
       type="button"
       className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${props.styles}`}
     >
-      Get Started
+      {props.title}
     </button>
   );
 };
