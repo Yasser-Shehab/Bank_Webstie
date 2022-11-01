0df0c52801cdb82f8f2d63b851707da2acb777c9
AquaApple <yassershehab9@gmail.com>
2022-11-01 22:24:57 +0200

    Added CTA & Clinets components

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client, index) => {
+          return (
+            <div
+              key={client.id}
+              className={`flex-1 ${styles.flexCenter}  sm:min-w-[192px] m-w-[120px] `}
+            >
+              <img
+                src={client.logo}
+                alt="client"
+                className={`sm:w-[192px] w-[100px] object-conetain  opacity-50 hover:opacity-100  ${
+                  index === 2 ? "mb-3" : "mb-0"
+                }`}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
